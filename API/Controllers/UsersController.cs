67abacfdfdeba159c8e15826b4e4c907c3bae5c3
khanhnd52 <khanh.nd5252@gmail.com>
2022-08-03 23:14:18 +0700

    End of section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-  [ApiController]
-  [Route("api/[controller]")]
-  public class UsersController : ControllerBase
+
+  public class UsersController : BaseApiController
   {
     private readonly DataContext _context;
     public UsersController(DataContext context)
@@ -20,18 +20,20 @@ namespace API.Controllers
 
     }
     [HttpGet]
+    [AllowAnonymous]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
-        var users = _context.Users.ToListAsync();
-        return await users;
+      var users = _context.Users.ToListAsync();
+      return await users;
     }
 
     //api/users/3
     [HttpGet("{id}")]
+    [Authorize]
     public async Task<ActionResult<AppUser>> GetUser(int id)
     {
-        return await _context.Users.FindAsync(id);
+      return await _context.Users.FindAsync(id);
     }
   }
-  
+
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,37 +1,45 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
-var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
+var _config = WebApplication.CreateBuilder(args).Configuration;
 
 //Add CORS
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-                      policy  =>
-                      {
-                          policy.WithOrigins("https://localhost:4200").AllowAnyHeader().AllowAnyMethod();
-                      });
+  options.AddPolicy(name: MyAllowSpecificOrigins,
+                    policy =>
+                    {
+                      policy.WithOrigins("https://localhost:4200").AllowAnyHeader().AllowAnyMethod();
+                    });
 });
 
-// Add services to the container.
 
+// Add services to the container.
+builder.Services.AddApplicationServices(_config);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(options => {
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+
 builder.Services.AddCors();
+builder.Services.AddIdentityServices(_config);
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -39,6 +47,8 @@ app.UseHttpsRedirection();
 app.UseRouting();
 
 app.UseCors(MyAllowSpecificOrigins);
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
